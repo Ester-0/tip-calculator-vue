1202dbf522db29f7b8699f2a4b9f43aec6b681b1
Ester-0 <azalea_@hotmail.com>
2023-11-08 21:38:40 +0100

    Ok

---
+++ app.js
@@ -0,0 +1,43 @@
+import {
+    createApp,
+    ref,
+  } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+  
+  createApp({
+    setup() {
+          
+          const billAmount = ref();
+          const percentageTip = ref();
+          const tipAmount = ref();
+          const total = ref();
+          
+          const calculate = () => {
+              tipAmount.value = billAmount.value*percentageTip.value/100; 
+
+              total.value = billAmount.value + tipAmount.value;
+
+              console.log("propina: ", tipAmount.value);
+              console.log("total: ", total.value);
+          };
+
+          
+                
+          
+
+      return {
+        
+          billAmount,
+          percentageTip,
+          calculate,
+          tipAmount,
+          total,
+          
+          
+          
+          
+          
+             
+          
+      };
+    },
+  }).mount("#app");
